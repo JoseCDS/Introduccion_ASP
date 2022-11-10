29eb23e064a8ef014c53e4ee7fdc33f2689be11d
JoseCDS <117650682+JoseCDS@users.noreply.github.com>
2022-11-10 13:22:10 -0300

    testeando
    testeando

--- TestASP/Sessiondeusuario/WebForm1.aspx.cs
+++ TestASP/Sessiondeusuario/WebForm1.aspx.cs
@@ -15,7 +15,7 @@ namespace Sessiondeusuario
         // del bowser 
         // y se consideraria la misma sesion
         // Si abrimoss en otro browser veremos que tenemos dos sesiones y una aplicacion instanciada
-
+        //hola mundo
 
         //Si no queremos tener cookie para nuestra sesion, en web
         protected void Page_Load(object sender, EventArgs e)
