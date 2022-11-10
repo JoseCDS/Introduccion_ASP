88b0cb25734e2b8337d288fc688c12d932446e82
JoseCDS <jtarrillo@cruzdelsur.com>
2022-11-10 15:39:12 -0300

    vv
    vv

--- TestASP/Sessiondeusuario/Global.asax.cs
+++ TestASP/Sessiondeusuario/Global.asax.cs
@@ -11,11 +11,34 @@ namespace Sessiondeusuario
 {
     public class Global : HttpApplication
     {
+
+        //Este handler se ejecuta cuando se crea una instancia de la aplicacion
         void Application_Start(object sender, EventArgs e)
         {
             // Código que se ejecuta al iniciar la aplicación
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //RouteConfig.RegisterRoutes(Routetable.Routes);
+            //BunleConfig.RegisterBundles(Bundletable.bundles
+            //Creamos varaibles de estado para la aplicacion
+            //Puede ser leida por todas las sesiones
+
+            Application["Aplicaciones"] = 0;
+            Application["SessionesUsuario"] = 0;
+
+            //Incrementamos 
+            Application["Aplicaciones"] = (int)Application["Aplicaciones"] + 1;
+
+          
+        }
+
+        //Este handler se ejecuta cuando se crea una sesion
+        void Session_Start(object sender, EventArgs e)
+        {
+            Application["SessionesUsuario"] = (int)Application["SessionesUsuario"] + 1;
+        }
+        //Este handler se ejecuta cuando finalza una sesion
+        void Session_End(object sender, EventArgs e)
+        {
+            Application["SessionesUsuario"] = (int)Application["SessionesUsuario"] - 1;
         }
     }
 }
